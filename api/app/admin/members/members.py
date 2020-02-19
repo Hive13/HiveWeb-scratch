4360ee54e32793dc323577252a773955b1079b52
Drew Kirkman <drew@drewkirkman.com>
2020-02-19 14:59:38 -0500

    move processing to centralized db instance

--- api/app/admin/members/members.py
+++ api/app/admin/members/members.py
@@ -2,35 +2,8 @@ from app import app
 import sqlalchemy
 import urllib.parse
 from flask import jsonify, request
-import db
 
-from app.creds import db_password, DEVICE, DEVICE_KEY
-
-#? is there a way for use to move this out of this file?
-# I think a lot of files will be using this data along with the `url` var and db connection. 
-# can we do this in __init__.py?
-user = "access"
-host = "honeycomb.at.hive13.org"
-dbname = "door"
-driver = "pg8000"
-
-# designate url
-# format is: dialect+driver://username:password@host:port/database
-url = "postgresql+{}://{}:{}@{}/{}".format(
-    driver,
-    user,
-    urllib.parse.quote_plus(db_password),
-    host,
-    dbname,
-)
-
-# create sqlalchemy engine
-# ? Should echo be True here?
-engine = sqlalchemy.create_engine(url, echo=True)
-
-# Bind to engine & connect (comment out for dev outisde of hive):
-db.metadata.bind = engine
-conn = engine.connect()
+from app import db_conn
 
 # member profile
 @app.route('/api/admin/members/profile/', methods=['GET', 'POST'])
@@ -46,7 +19,7 @@ def members():
     
     # try to execute the query, except when it returns an error.
     try:
-        result = conn.execute(s, x=str(member_id))
+        result = db_conn.execute(s, x=str(member_id))
         rows = [dict(row) for row in result]
         return jsonify(rows)
     except:
